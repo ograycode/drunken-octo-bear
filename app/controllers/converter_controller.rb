9cf419d3a5440a45c0d28d4dfb768d0cc981df92
Keith Bennett <keithrbennett@gmail.com>
2012-07-08 17:12:29 -0400

    Fleshed out converter controller, and put calculated result on result page.

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,14 +1,55 @@
+require 'converter_helper'
+
 class ConverterController < ApplicationController
 
-#http://localhost:3000/converter/convert?utf8=%E2%9C%93&input=test&unit=Fahrenheit&commit=Convert
+  include ConverterHelper
+
+  VALID_DIRECTION_STRINGS = %w(to_f to_c)
+
   def convert
-
-    if params[:unit] == 'Fahrenheit'
-
-    end
-
+    validate
+    @answer = direction == 'to_c' ? f_to_c(input) : c_to_f(input)
+    @direction_caption = direction_description_string
   end
 
 
+  def validate
+    validate_direction_string(direction)
+    validate_temperature_input
+  end
+
+
+  def validate_direction_string(string)
+    unless VALID_DIRECTION_STRINGS.include?(string)
+      raise "Application error: unit parameter has an invalid value (not in #{VALID_DIRECTION_STRINGS.join(', ')})."
+    end
+  end
+
+
+  def validate_temperature_input
+    unless numeric?(params[:input])
+      raise "Temperature input must be a number."
+    end
+  end
+
+
+  def direction
+    params[:direction]
+  end
+
+
+  def input
+    params[:input].to_f
+  end
+
+
+  def numeric?(object)
+    true if Float(object) rescue false
+  end
+
+
+  def direction_description_string
+    (direction == 'to_f') ? "to Fahrenheit" : "to Celsius"
+  end
 end
 
