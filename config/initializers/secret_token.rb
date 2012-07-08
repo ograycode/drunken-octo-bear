822e67eb0b801da2246ff95097a5a0096009ed18
Keith Bennett <keithrbennett@gmail.com>
2012-07-08 15:19:45 -0400

    Initial Commit with welcom#index controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrunkenOctoBear::Application.config.secret_token = '7e2cb7302307ea33f861548af9cb23921afa5bce98cb85f8b1674092d62c8329484e43533fb5fe2e5e179eeddef67dc2653f65d64d039f55aa4202c474e3cb43'
